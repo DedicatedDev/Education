a831ce0b93e47b8151dccbf2279ccc1d7f083a6d
DedicatedDev <=>
2022-01-22 23:51:20 +0300

    start contract

---
+++ contracts/Election.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+contract Election {
+    struct Voter {
+        uint weight;
+        bool voted;
+        address delegate;
+        uint vote;
+    }
+
+    struct Proposal {
+        bytes32 name;
+        uint voteCount;
+    }
+
+    address public chairperson;
+    mapping(address => Voter) public voters;
+    Proposal[] public proposals;
+    
+
+}
